e4e306280bab425d3c154da8a304b6906e4ca8d4
emil <bgemo105@gmail.com>
2023-06-12 23:01:29 +0300

    full server drop routine

--- drop_server.sh
+++ drop_server.sh
@@ -2,3 +2,4 @@ kubectl delete service cassandra-service server-service
 kubectl delete deployment cassandra-deployment server-deployment
 sleep 45s
 docker rmi image-fetcher
+kubectl delete hpa server-deployment
