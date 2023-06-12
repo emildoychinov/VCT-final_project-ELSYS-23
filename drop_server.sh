185f417ec017d5b412cb2fd172ae27e2b6738da7
emil <bgemo105@gmail.com>
2023-06-12 22:12:03 +0300

    purva stupka v kluba chuvam bum bim bam bem

--- drop_server.sh
+++ drop_server.sh
@@ -1,2 +1,4 @@
 kubectl delete service cassandra-service server-service
 kubectl delete deployment cassandra-deployment server-deployment
+sleep 45s
+docker rmi image-fetcher
--- init_server.sh
+++ init_server.sh
@@ -3,3 +3,4 @@ kubectl apply -f cassandra.yaml
 sleep 120s
 echo "Cassandra running!"
 kubectl apply -f server_deployment.yaml
+kubectl autoscale deployment server-deployment --cpu-percent=50 --min=1 --max=10
